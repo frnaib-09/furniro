4c00250ba52020287a05217d93405847116d6a7c
Foyjur Rafee <frnaib9@gmail.com>
2026-09-15 14:47:11 +0600

    shop filter 25% done

--- src/components/PageHeader.jsx
+++ src/components/PageHeader.jsx
@@ -4,10 +4,10 @@ import { MdKeyboardArrowRight } from "react-icons/md";
 
 const PageHeader = ({ title }) => {
   return (
-    <div>
-        <img src="/images/pages bg.png" alt="" className="w-full h-full object-cover" />
-        <h1>{title}</h1>
-        <p>Home <MdKeyboardArrowRight /> {title}</p>
+    <div className='bg-[url("/images/pages-bg.png")] bg-cover bg-center bg-no-repeat min-h-79 flex flex-col justify-center items-center gap-4'>
+        <img src="/images/favicon.png" alt="" className="" />
+        <h1 className='font-primary font-medium text-5xl text-black'>{title}</h1>
+        <p className='flex items-center gap-3 font-primary font-light text-base text-black mt-0.5'><span className='flex items-center gap-3 font-medium'>Home <MdKeyboardArrowRight /> </span>{title}</p>
     </div>
   )
 }
